9761e27412b88bee2b3c2c335d8bfa850f81888e
Erion V <erion-v@hotmail.co.uk>
2014-04-10 17:44:43 +0100

    changed email message for activation

--- app/views/emails/auth/activate.blade.php
+++ app/views/emails/auth/activate.blade.php
@@ -1,6 +1,6 @@
 Dear {{ $username }},
 <br><br>
-An account has been created for you at LOCALHOST
+An account has been created for you at ezaria.co.uk (carComp)
 <br><br>
 Please visit this url to activate your account.
 <br><br>

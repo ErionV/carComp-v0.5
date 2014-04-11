0753f4a631c4882a901269d70be6084ccd1da848
Erion V <erion-v@hotmail.co.uk>
2014-04-11 01:30:02 +0100

    added production and local variables

---
+++ app/config/local/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'connection' => [
+        'mysql' => [
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            //'unix_socket'   => '/Applications/MAMP/tmp/mysql/mysql.sock',
+            'database'  => 'carComp',
+            'username'  => 'root',
+            'password'  => 'root',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ]
+    ]
+];

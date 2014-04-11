20c190feb00f5f7d9bbca1f99d1a296d2137dd37
Erion V <erion-v@hotmail.co.uk>
2014-04-11 17:40:40 +0100

    .

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -1,14 +1,14 @@
 <?php
 
 return [
-    'connection' => [
 
+    'connections' => [
         'mysql' => [
-			'host'      => '127.0.0.1',
+            'host'      => '127.0.0.1',
             'unix_socket'   => '/Applications/MAMP/tmp/mysql/mysql.sock',
-			'database'  => 'carComp',
-			'username'  => 'root',
-			'password'  => 'root',
+            'database'  => 'carComp',
+            'username'  => 'root',
+            'password'  => 'root',
         ]
     ]
 ];
